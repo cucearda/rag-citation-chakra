1c5930849c1cf8384aa3d70b5f9cf7104d175ae5
cucearda <cuce.arda1999@gmail.com>
2026-03-18 23:51:12 +0100

    feat: remount ConversationView on project switch for isolated chat history

--- src/layouts/project-layout.tsx
+++ src/layouts/project-layout.tsx
@@ -1,14 +1,16 @@
 import { HStack, Box } from "@chakra-ui/react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useParams } from "react-router-dom"
 import ProjectSidebar from "@/components/ui/ProjectSidebar"
 import { ProjectProvider } from "@/context/ProjectContext"
 
 export default function ProjectLayout() {
+  const { projectId } = useParams<{ projectId: string }>()
+
   return (
     <ProjectProvider>
       <HStack flex="1" minH="0" gap="0" alignItems="stretch">
         <ProjectSidebar />
-        <Box flex="1" minH="0" display="flex" flexDirection="column">
+        <Box key={projectId} flex="1" minH="0" display="flex" flexDirection="column">
           <Outlet />
         </Box>
       </HStack>
